8079629aa005902161ed4ff444c09ad5d20a08b7
Juan B Cabral <jbc.develop@gmail.com>
2017-12-28 01:56:03 -0300

    things

---
+++ day6/oTree/urls.py
@@ -0,0 +1,7 @@
+# urls.py
+from django.conf.urls import url, include
+from otree.default_urls import urlpatterns
+
+urlpatterns.append(
+    url(r'^transcription/', include('real_effor1.ajax', namespace="real_effort1"))
+)
